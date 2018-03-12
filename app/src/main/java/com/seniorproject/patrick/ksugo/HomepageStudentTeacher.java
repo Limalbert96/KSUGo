c4076f2c4cb75a0aede49ba92758d7f13bd151f1
Patrick Hilerio <33403070+philerio@users.noreply.github.com>
2018-03-12 14:13:19 -0400

    Update HomepageStudentTeacher.java

--- app/src/main/java/com/seniorproject/patrick/ksugo/HomepageStudentTeacher.java
+++ app/src/main/java/com/seniorproject/patrick/ksugo/HomepageStudentTeacher.java
@@ -1,8 +1,8 @@
 package com.seniorproject.patrick.ksugo;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
 
@@ -16,7 +16,6 @@ public class HomepageStudentTeacher extends AppCompatActivity {
     private ImageButton InteractiveMap;
     private ImageButton News;
     private ImageButton Events;
-    private ImageButton Logout;
     private ImageButton Emergency;
 
     @Override
@@ -25,18 +24,17 @@ public class HomepageStudentTeacher extends AppCompatActivity {
         setContentView(R.layout.activity_homepagestudentteacher);
 
         D2L = (ImageButton) findViewById(R.id.D2LButton);
-      /*  OwlLife = (ImageButton) findViewById(R.id.OwlLifeButton);
+        OwlLife = (ImageButton) findViewById(R.id.OwllifeButton);
         Handshake = (ImageButton) findViewById(R.id.HandshakeButton);
         BOB = (ImageButton) findViewById(R.id.BOBButton);
         ContactDirectory = (ImageButton) findViewById(R.id.ContactDirectoryButton);
         InteractiveMap = (ImageButton) findViewById(R.id.CampusMap);
         News = (ImageButton) findViewById(R.id.NewsButton);
         Events = (ImageButton) findViewById(R.id.EventsButton);
-        Logout = (ImageButton) findViewById(R.id.LogoutButton);*/
-        Emergency = (ImageButton) findViewById(R.id.emergency);
+        Emergency = (ImageButton) findViewById(R.id.emergencyButton);
 
 
-        //Still Need to implement these.
+        // D2L - Still Need to implement these.
         D2L.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -44,21 +42,47 @@ public class HomepageStudentTeacher extends AppCompatActivity {
             }
         }); //Need a method to get User Type.
 
+        // Emergency
         Emergency.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(HomepageStudentTeacher.this, EmergencyActivity.class));
             }
-        }); //Need a method to get User Type.
+        });
 
+        // Directory
+        ContactDirectory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HomepageStudentTeacher.this, Directory.class));
+            }
+        });
 
-      /*  Handshake.setOnClickListener(new View.OnClickListener() {
+        // NewsFeed
+        News.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HomepageStudentTeacher.this, NewsFeed.class));
+            }
+        });
+
+        // Owl Life
+        OwlLife.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
             }
         });
 
+        // Handshake
+        Handshake.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        // BOB
         BOB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -66,13 +90,7 @@ public class HomepageStudentTeacher extends AppCompatActivity {
             }
         });
 
-        ContactDirectory.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
+        // Maps
         InteractiveMap.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -80,13 +98,8 @@ public class HomepageStudentTeacher extends AppCompatActivity {
             }
         }); //Need User Type
 
-        News.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
 
-            }
-        });
-
+        // Events
         Events.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -94,13 +107,7 @@ public class HomepageStudentTeacher extends AppCompatActivity {
             }
         });
 
-        Logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(HomepageStudentTeacher.this, Login.class);
-                startActivity(intent);
-            }
-        });*/
+
     }
 }
 
