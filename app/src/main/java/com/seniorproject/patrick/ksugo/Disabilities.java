cfbd4d8caf2360ad7c37d0f49464e756b34569f6
chaseAero <aerohead35@gmail.com>
2018-04-17 00:26:58 -0400

    Added honors component

--- app/src/main/java/com/seniorproject/patrick/ksugo/Disabilities.java
+++ app/src/main/java/com/seniorproject/patrick/ksugo/Disabilities.java
@@ -9,16 +9,6 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
-import android.widget.ListView;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import twitter4j.Status;
-import twitter4j.Twitter;
-import twitter4j.TwitterException;
-import twitter4j.TwitterFactory;
-import twitter4j.conf.ConfigurationBuilder;
 
 public class Disabilities extends AppCompatActivity {
 
